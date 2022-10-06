c25e9654b7265401f5a07e056193dfb1f6146b38
Roman Bogoudinov <r.bogoudinov@mac.com>
2022-10-06 11:03:10 +0300

    build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useState, useEffect } from 'react'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
