7cfefbd8a5ef76874417e2b716266715e03992fd
Roman Bogoudinov <r.bogoudinov@mac.com>
2022-10-06 10:32:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Users } from './Users';
+import CardXL from './CardXL';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Users/>,
+  },
+
+  {
+    path: "/:userId",
+    element: <CardXL/>,
+  },
+]);
+
+const App = () => (
+  <RouterProvider router={router} />
+)
+
+export default App
